202ae5c1f23218f945c09308a939a815dca6014b
unknown <assgueye858@gmail.com>
2026-06-25 18:07:02 +0000

    config: ajout de la configuration CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     "rest_framework",
     "drf_spectacular",
     "django_filters",
+     "corsheaders",
 
     "accounts",
     "catalog",
     "loans",
     "dashboard",
+   
 
 
     
@@ -53,12 +55,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'config.urls'
