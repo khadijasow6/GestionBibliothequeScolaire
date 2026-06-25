923a882a1e725d9087539f2eb411fcc5cff4504b
unknown <assgueye858@gmail.com>
2026-06-25 18:13:41 +0000

    test: ajout du test de retour des emprunts

--- backend/loans/tests.py
+++ backend/loans/tests.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 
 from django.test import TestCase
 from django.utils import timezone
+from rest_framework.test import APIClient
 
 from accounts.models import User
 from catalog.models import Book, BookCopy, Category
@@ -11,8 +12,10 @@ from .models import Loan
 from .services import update_overdue_loans
 
 
-class OverdueLoanTest(TestCase):
+class LoanTest(TestCase):
     def setUp(self):
+        self.client = APIClient()
+
         self.admin = User.objects.create_user(
             username="testadmin",
             password="TestPassword123!",
@@ -59,5 +62,33 @@ class OverdueLoanTest(TestCase):
         self.assertEqual(
             loan.status,
             Loan.Status.EN_RETARD,
+        )
+
+    def test_return_loan_updates_book_copy_status(self):
+        loan = Loan.objects.create(
+            student=self.student,
+            book_copy=self.book_copy,
+            due_at=timezone.now() + timedelta(days=7),
+            status=Loan.Status.EN_COURS,
+            created_by=self.admin,
+        )
+
+        self.client.force_authenticate(user=self.admin)
+
+        response = self.client.post(
+            f"/api/loans/{loan.id}/return/",
+        )
+
+        loan.refresh_from_db()
+        self.book_copy.refresh_from_db()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            loan.status,
+            Loan.Status.RETOURNE,
+        )
+        self.assertEqual(
+            self.book_copy.status,
+            BookCopy.Status.DISPONIBLE,
         )
 
